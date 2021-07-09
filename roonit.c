ac28e2d7c107fd95831c37b8e00ebd40edbad82c
19742bytes <87100084+19742bytes@users.noreply.github.com>
2021-07-09 10:39:49 -0700

    Update roonit.c

--- roonit.c
+++ roonit.c
@@ -17,7 +17,7 @@ const char dir[] = "/var/service";
 char sv[256];
 
 int main(int argc, char *argv[]) {
-	if (argc < 2 || argc > 3) { //If this OR that
+	if (argc != 2) { //If this OR that
 		help();
 		exit(1);
 	}
